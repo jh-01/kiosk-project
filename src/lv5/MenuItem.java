04b5dbdecf1e73bf71144833142f47841ad52ecb
jh-01 <were1117@sm.ac.kr>
2025-04-25 10:52:37 +0900

    feat: Lv5 캡슐화 적용
    - MenuItem, Menu 그리고 Kiosk 클래스의 필드에 직접 접근하지 못하도록 설정
    - Getter와 Setter 메서드를 사용해 데이터를 관리
    
    Closes : #5

---
+++ src/lv5/MenuItem.java
@@ -0,0 +1,30 @@
+package lv5;
+
+public class MenuItem {
+    private final String name;
+    private final int cost;
+    private final String desc;
+
+    public MenuItem(String name, int cost, String desc){
+        this.name = name;
+        this.cost = cost;
+        this.desc = desc;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s | W %.1f | %s", name, (double) cost/1000, desc);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+}
