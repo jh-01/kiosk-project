1a2ed54918ffde595cdb24b3a0e84800d3af8896
jh-01 <were1117@sm.ac.kr>
2025-04-28 10:47:46 +0900

    refactor: Lv1 리팩토링
    - 문자 입력시 예외처리
    - 숫자를 입력할 때까지 반복처리
    - 입력을 받는 상태를 보여주기 위한 > 출력
    - 주석 추가
    
    Closes : #8

--- src/lv1/App.java
+++ src/lv1/App.java
@@ -1,12 +1,14 @@
 package lv1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/* Lv 1. 기본적인 키오스크 */
 public class App {
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args){
-        while(true){
+        while(true){ // 0이 입력되기 전까지 반복 수행
             int choice = selectMenu();
             switch (choice){
                 case 1: System.out.println("ShackBurger 나왔습니다!"); break;
@@ -19,6 +21,7 @@ public class App {
         }
     }
 
+    // 메뉴 출력 후 사용자의 입력을 받는 메서드
     private static int selectMenu(){
         System.out.println("[ SHAKESHACK MENU ]");
         System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
@@ -26,6 +29,16 @@ public class App {
         System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
         System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
         System.out.println("0. 종료      | 종료");
-        return sc.nextInt();
+
+        while (true){
+            System.out.print("> ");
+            try {
+                return sc.nextInt();
+            }
+            catch (InputMismatchException e){ // 예외 처리 : 문자를 입력한 경우
+                sc.nextLine();
+                System.out.println("숫자를 입력해주세요!");
+            }
+        }
     }
 }
